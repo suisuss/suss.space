5b5a70e2004732a42a8807cd2a14a018da6fbbc7
Jacob Sussmilch <59195637+suisuss@users.noreply.github.com>
2019-12-30 22:09:33 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flaskapp.config import Config
+from .config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
